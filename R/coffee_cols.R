7c12216382a0f15864ad51b9c664191c0e8d806b
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-09-13 18:45:09 +0200

    added new coffee colors from hanno rein
    thanks Maelle Salmon @maelle

--- R/coffee_cols.R
+++ R/coffee_cols.R
@@ -45,8 +45,8 @@ input_to_ints <- function(...) {
 }
 
 char_to_int <- function(col_vec_el, col_df) {
-    lookup_list <- rep(1:10, 2)
-    names(lookup_list) <- c(as.character(1:10), col_df$color_name)
+    lookup_list <- rep(seq_len(nrow(col_df)), 2)
+    names(lookup_list) <- c(as.character(col_df$color_int), col_df$color_name)
     if (!col_vec_el %in% names(lookup_list)) {
         stop("Invalid color name or color number", call. = FALSE)
     }
@@ -55,7 +55,7 @@ char_to_int <- function(col_vec_el, col_df) {
 
 get_cols_data_frame <- function() {
     data.frame(
-        color_int  = 1:10,
+        color_int  = 1:44,
         color_name = c("mocha_splatter",
                        "coffee_grounds",
                        "espresso_cream",
@@ -65,7 +65,41 @@ get_cols_data_frame <- function() {
                        "cream",
                        "cream_and_coffee",
                        "beans",
-                       "espresso"),
+                       "espresso",
+                       "coffee1",
+                       "coffee2",
+                       "coffee3",
+                       "coffee4",
+                       "coffee5",
+                       "coffee6",
+                       "coffee7",
+                       "coffee8",
+                       "coffee9",
+                       "coffee10",
+                       "coffee11",
+                       "coffee12",
+                       "coffee13",
+                       "coffee14",
+                       "coffee15",
+                       "coffee16",
+                       "coffee17",
+                       "coffee18",
+                       "coffee19",
+                       "coffee20",
+                       "coffee21",
+                       "coffee22",
+                       "coffee23",
+                       "coffee24",
+                       "coffee25",
+                       "coffee26",
+                       "coffee27",
+                       "coffee28",
+                       "coffee29",
+                       "coffee30",
+                       "coffee31",
+                       "coffee32",
+                       "coffee33",
+                       "coffee34"),
         hex_code   = c("#4D351D",
                        "#8B6E46",
                        "#B99768",
@@ -75,8 +109,45 @@ get_cols_data_frame <- function() {
                        "#DAC189",
                        "#B68D3D",
                        "#894200",
-                       "#2C1901"),
+                       "#2C1901",
+                       "#CEAF9A",
+                       "#C59470",
+                       "#B78B67",
+                       "#AC724F",
+                       "#A3561C",
+                       "#922904",
+                       "#580E03",
+                       "#DCC6B4",
+                       "#CEAF98",
+                       "#C3946D",
+                       "#B78D68",
+                       "#AB724F",
+                       "#9F5A22",
+                       "#8A2E09",
+                       "#660F02",
+                       "#FBD9C5",
+                       "#F8CCAF",
+                       "#FBE8D1",
+                       "#EDD5CC",
+                       "#F7CEAA",
+                       "#E2AC97",
+                       "#F5AC72",
+                       "#ED924E",
+                       "#F48F25",
+                       "#D37152",
+                       "#EF6D0F",
+                       "#EA500F",
+                       "#D24D12",
+                       "#E6300C",
+                       "#D02D10",
+                       "#CC1906",
+                       "#AF1506",
+                       "#900F06",
+                       "#780D07"),
         stringsAsFactors = FALSE
     )
 }
+
+
+
 
--- tests/testthat/test_coffee_cols.R
+++ tests/testthat/test_coffee_cols.R
@@ -2,7 +2,6 @@ context("coffee_cols function")
 test_that("coffee_cols works on single input", {
   expect_equal(coffee_cols(latte), "#CCB697")
   expect_equal(coffee_cols(4), "#CCB697")
-  expect_error(coffee_cols(11), "Invalid color name or color number")
   expect_error(coffee_cols(lette), "Invalid color name or color number")
 })
 
@@ -10,7 +9,5 @@ test_that("coffee_cols works on single input", {
   expect_equal(coffee_cols(latte, espresso), c("#CCB697", "#2C1901"))
   expect_equal(coffee_cols(4, 10), c("#CCB697", "#2C1901"))
   expect_equal(coffee_cols(latte, 10), c("#CCB697", "#2C1901"))
-  expect_error(coffee_cols(4, 11), "Invalid color name or color number")
   expect_error(coffee_cols(espresso, lette), "Invalid color name or color number")
-  expect_error(coffee_cols(espresso, 11), "Invalid color name or color number")
 })
